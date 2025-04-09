5f69d25b5f5b959507037361c8bdd7ca14c72f38
RaghavendraKushtagi <raghukushtagi1212@gmail.com>
2025-04-09 13:10:50 +0530

    Refactor vite.config.ts for consistent formatting and improved readability

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 // @ts-ignore
 import eslint from 'vite-plugin-eslint';
-import { defineConfig } from "vite"
- 
+
 export default defineConfig({
-  plugins: [react(),eslint()],
+  plugins: [react(), eslint()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
